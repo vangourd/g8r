516ca45acf898f1ff3233d47c81136ccaa15c09c
Brian Logan <mail@brianjlogan.com>
2023-11-23 22:27:04 -0500

    Working on IacSync object

--- experiments/snapper/src/utils/repo.rs
+++ experiments/snapper/src/utils/repo.rs
@@ -12,7 +12,7 @@ pub struct IacSync {
     config: utils::config::Config,
 }
 
-pub impl IacSync {
+impl IacSync {
     pub fn new(config: utils::config::Config) -> IacSync {
         return IacSync {
             config: config,
@@ -20,111 +20,99 @@ pub impl IacSync {
         }
     }
 
-    pub fn init_repo(&self){
 
+    pub fn init(&self) {
+
+        let repo_path = "./iac/";
+
+        if !Path::exists(Path::new(&repo_path)) {
+
+            let mut configured_url = Url::parse(&url)
+                .expect("Unable to parse URL");
+
+            configured_url.set_username(&username)
+                .expect("Unable to set username");
+            configured_url.set_password(Some(&token))
+                .expect("Unable to set password");
+            
+
+            error!("Configured URL: {}",&configured_url);
+
+            let repo= Repository::clone(&configured_url.as_str(), &repo_path)
+                .expect("Unable to clone repository");
+            info!("Cloned repository {}",&url);
+            Ok(repo)
+        } else {
+            let repo = Repository::open("./iac/")
+                .expect("Unable to open existing repository path");
+            fetch(&repo).expect("Unable to fetch from repo");
+
+            return Ok(repo)
+        }
+
+        
     }
 
-    pub fn sync(){
 
+    pub fn check_local(&self){
+        // Check if local repository exists
+        if !Path::exists(Path::new(&self.config.local_path)) {
+            let repo = clone_repo(&self.config)?;
+            
+        } else {
+            let repo = Repository::open("./iac/")
+                .expect("Unable to open existing repository path");
+            fetch(&repo).expect("Unable to fetch from repo");
+
+        }
     }
-}
+    pub fn clone_repo(&self) -> Result<Repository, Error> {
 
-#[derive(Default)]
-struct IacSyncBuilder{
-
-
-
-}
-
-// Check if local repository exists
-if !Path::exists(Path::new(&config.local_path)) {
-    let repo = clone_repo(config)?;
-    return Ok(IacSync {
-        local: repo,
-        config: config,
-    })
-} else {
-    let repo = Repository::open("./iac/")
-        .expect("Unable to open existing repository path");
-    fetch(&repo).expect("Unable to fetch from repo");
-
-}
-
-pub fn clone_repo(config: utils::config::Config) -> Result<Repository, Error> {
-    let mut configured_url = Url::parse(&config.repo)
-        .expect("Unable to parse URL");
-
-    configured_url.set_username(&config.username)
-        .expect("Unable to set username");
-    configured_url.set_password(Some(&config.token))
-        .expect("Unable to set password");
-    
-    error!("Configured URL: {}",&configured_url);
-
-    let repo= Repository::clone(&configured_url.as_str(), &config.local_path)
-        .expect("Unable to clone repository");
-    info!("Cloned repository {}",&config.repo);
-    Ok(repo)
-}
-
-pub fn initialize(url: String, token: String, branch: String ,tag: String, username: String) -> Result<Repository, std::fmt::Error> {
-
-    let repo_path = "./iac/";
-
-    if !Path::exists(Path::new(&repo_path)) {
-
-        let mut configured_url = Url::parse(&url)
+        let mut configured_url = Url::parse(&self.config.repo)
             .expect("Unable to parse URL");
-
-        configured_url.set_username(&username)
+    
+        configured_url.set_username(&self.config.username)
             .expect("Unable to set username");
-        configured_url.set_password(Some(&token))
+        configured_url.set_password(Some(&self.config.token))
             .expect("Unable to set password");
         
-
         error!("Configured URL: {}",&configured_url);
-
-        let repo= Repository::clone(&configured_url.as_str(), &repo_path)
+    
+        let repo= Repository::clone(&configured_url.as_str(), &self.config.local_path)
             .expect("Unable to clone repository");
-        info!("Cloned repository {}",&url);
+        info!("Cloned repository {}",&self.config.repo);
         Ok(repo)
-    } else {
-        let repo = Repository::open("./iac/")
-            .expect("Unable to open existing repository path");
-        fetch(&repo).expect("Unable to fetch from repo");
-
-        return Ok(repo)
     }
 
+    pub fn fetch(repo: &Repository) -> Result<(), git2::Error> {
+
+        info!("Fetching remote");
+        repo.find_remote("origin")
+            .expect("Unable to find remote")
+            .fetch(&["main"], None, None)
+        
+    }
+
+
+    // Function to hard reset the current branch to 'origin/main
+    pub fn reset(repo: &Repository) -> Result<(), git2::Error> {
+
+        info!("Resetting repository");
+        
+        // Locate the commit object for 'origin/main'; 
+        let target_commit = repo.find_reference("origin/main")?.peel(ObjectType::Commit)?;
+
+        // Create a CheckoutBuilder for configuring the hard reset
+        // 'force()' ensures that all changes in the working directory are overritten
+        let mut checkout_opts = CheckoutBuilder::new();
+
+        // Perform the hard reset
+        // This moves HEAD to 'origin/main', resets the index, and updates the working directory
+        repo.reset(
+            &target_commit,
+            ResetType::Hard,
+            Some(&mut checkout_opts.force()),
+        )
+    }
     
-}
-
-pub fn fetch(repo: &Repository) -> Result<(), git2::Error> {
-
-    info!("Fetching remote");
-    repo.find_remote("origin")
-        .expect("Unable to find remote")
-        .fetch(&["main"], None, None)
-    
-}
-
-// Function to hard reset the current branch to 'origin/main
-pub fn reset(repo: &Repository) -> Result<(), git2::Error> {
-
-    info!("Resetting repository");
-    
-    // Locate the commit object for 'origin/main'; 
-    let target_commit = repo.find_reference("origin/main")?.peel(ObjectType::Commit)?;
-
-    // Create a CheckoutBuilder for configuring the hard reset
-    // 'force()' ensures that all changes in the working directory are overritten
-    let mut checkout_opts = CheckoutBuilder::new();
-
-    // Perform the hard reset
-    // This moves HEAD to 'origin/main', resets the index, and updates the working directory
-    repo.reset(
-        &target_commit,
-        ResetType::Hard,
-        Some(&mut checkout_opts.force()),
-    )
 }
