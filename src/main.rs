745322686f0c564d90f1bf0fce324d52267e5a63
Brian Logan <mail@brianjlogan.com>
2023-11-25 10:02:30 -0500

    Hoisting snapper to g8r to be main setup

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,35 @@
-use std::fs;
-use std::sync::{Arc, Mutex};
-use toml::Value;
-use tokio::fs as tokio_fs;
-use tokio::sync::Semaphore;
+use std::thread::sleep;
+use std::time::Duration;
+
+use tokio;
+
+mod utils;
+use utils::config::Config;
+use utils::repo;
+
+use log::{info, warn, error, log_enabled, Level};
 
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+    //print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    info!("Starting snapper ...");
 
-    let semaphore = Arc::new(Semaphore::new(1));
-    
-    let shared_toml = Arc::new(Mutex::new(None));
+    let config = Config::from_file("config.yaml")
+                    .expect("Failed to load config");
 
-    // Spawn a tokio task to read and parse the toml file
-    let read_task = tokio::spawn(({
-        let semaphore = semaphore.clone();
-        let shared_toml = shared_toml.clone();
-        async move {
-            // Acquire the semaphore to gain exclusive access to shared_toml
-            let _permit = semaphore.acquire().await.unwrap();
+    println!("Initating reconciliation loop every {}",config.refresh);
 
-            // Read the TOML file into a string (use your actual file path)
-            let toml_str = tokio_fs::read_to_string("config.toml")
-                .await
-                .expect("Failed to read configuration file toml");
-            
-            let toml_value: Value = toml::de::from_str(&toml_str).expect("Failed to parse TOML");
+    let _iac = repo::IacSync::new(config)
+        .init();
 
-            // Store the parsed TOML data in the shared data structure
-            *shared_toml.lock().unwrap() = Some(toml_value);
-        }
-    }));
+    loop{
+        sleep(Duration::new(5,0));
+        println!("Done...");
+        sleep(Duration::new(30,0));
+    }
 
-    let access_task = tokio::spawn({
-        let semaphore = semaphore.clone();
-        let shared_toml = shared_toml.clone();
-        async move {
-            // Acquire the semaphore to gain access to shared_toml
-            let _permit = semaphore.acquire().await.unwrap();
-            
-            // Access the parsed TOML data and print the value
-            let mutex_guard = shared_toml.lock().unwrap();
-            let toml_value = mutex_guard.as_ref().unwrap();
-            let mode = toml_value["mode"].as_str().expect("Missing or invalid mode field");
-            println!("mode: {}", mode);
-        }
-    });
-
-    tokio::join!(read_task, access_task);
-
+    // Evaluate IAC rules for host
 
 }
